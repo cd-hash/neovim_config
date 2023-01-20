fe757c9a136f8748589ce818893b32903f72d872
chris <cdresher2010@gmail.com>
2023-01-20 11:06:27 -0500

    Changes to be committed: 	modified:   after/plugin/nvim_tree.rc.lua  - <leader>tt to toggle nvim_tree

--- after/plugin/nvim_tree.rc.lua
+++ after/plugin/nvim_tree.rc.lua
@@ -2,3 +2,8 @@ local status, nvim_tree = pcall(require, "nvim-tree")
 if (not status) then return end
 
 nvim_tree.setup()
+
+vim.keymap.set(
+  'n', '<leader>tt', ':NvimTreeToggle<CR>', {
+  noremap = true
+})
