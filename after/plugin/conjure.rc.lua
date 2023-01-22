242c03025967afc787db330d3067ac3e9b267661
chris <cdresher2010@gmail.com>
2023-01-22 16:30:20 -0500

    Changes to be committed: 	modified:   after/plugin/conjure.rc.lua  - fixed not working conjure config

--- after/plugin/conjure.rc.lua
+++ after/plugin/conjure.rc.lua
@@ -1,5 +1,5 @@
 local status, conjure = pcall(require, "conjure")
 if (not status) then return end
 
-vim.g.conjure['client']['clojure']['nrepl']['eval']['auto_require'] = false
-vim.g.conjure['client']['clojure']['nrepl']['connection']['auto_repl']['enabled'] = false
+vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
+vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
