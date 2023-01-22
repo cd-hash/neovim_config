493c0644d57190626f2bfcc64b799a136b8d49d9
chris <cdresher2010@gmail.com>
2023-01-22 16:30:57 -0500

    Changes to be committed: 	modified:   after/plugin/autopairs.rc.lua  - realized I just needed to disable autopairs for Clojure rather than    turning off line level paren checking

--- after/plugin/autopairs.rc.lua
+++ after/plugin/autopairs.rc.lua
@@ -2,6 +2,5 @@ local status, autopairs = pcall(require, "nvim-autopairs")
 if (not status) then return end
 
 autopairs.setup({
-  disable_filetype = { "TelescopePrompt", "vim" },
-  enable_check_bracket_line = false,
+  disable_filetype = { "TelescopePrompt", "vim", "clojure" },
 })
